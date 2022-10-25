907a414441b9fffb3ab606b50ac7bb677441442a
AlexandreJareck <alexandrejareck@gmail.com>
2022-10-24 21:28:05 -0300

    feat: created order item dto

---
+++ src/AppCrud.Api/DTOs/OrderItemDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppCrud.Api.DTOs;
+
+public class OrderItemDTO
+{
+    public Guid Id { get; set; }
+    public Guid OrderId { get; set; }
+    public Guid ProductId { get; set; }
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    public int Quantity { get; set; }
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    public decimal? UnitPrice { get; set; }
+
+    public OrderDTO Order { get; set; }
+    public ProductDTO Product { get; set; }
+}
